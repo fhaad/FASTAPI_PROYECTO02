3b2f52c25c32ecefe504f1f4dfd0a8246d4855f2
fhaad <fhaad25@gmail.com>
2022-12-18 08:48:26 -0500

    cambio

--- app/ejemplo01.py
+++ app/ejemplo01.py
@@ -5,8 +5,8 @@ app = FastAPI(title="Practica01", description="Formulario", version="0.1")
 @app.get("/ejemplo")
 async def ejemplo(nombre:str, apellido:str):
 
-    {"nombre":nombre}
-    {"apellido":apellido}
+    #{"nombre":nombre}
+    #{"apellido":apellido}
 
 
 
