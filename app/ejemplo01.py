ef0284062b7c05fb75b96284004cef22a65be46b
fhaad <fhaad25@gmail.com>
2022-12-18 09:18:07 -0500

    cambio

--- app/ejemplo01.py
+++ app/ejemplo01.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Practica01", description="Formulario", version="0.1")
 
+user_db = { 
+
+    'jack':{'username':'jack', 'dated_joined':'2022-01-01', 'location':'New York', 'age':'28'},
+    'joe':{'username':'joe', 'dated_joined':'2022-02-01', 'location':'Memphis', 'age':'25'},
+    'jane':{'username':'jane', 'dated_joined':'2022-03-01', 'location':'Miami', 'age':'30'}
+
+
+}
+
+
 @app.get("/ejemplo")
-async def ejemplo(nombre:str, apellido:str):
+async def ejemplo(nombres:str, apellidos:str, cellphone:str):
 
     #{"nombre":nombre}
     #{"apellido":apellido}
 
 
 
-    return {"nombre":str(nombre), "apellido":str(apellido)}
+    return {"nombre":str(nombres), "apellido":str(apellidos), "celular":str(cellphone)}
 
