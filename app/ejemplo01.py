ef262766cd13d9cc922600f749c83d6853deb8c3
fhaad <fhaad25@gmail.com>
2022-12-18 09:27:50 -0500

    cambio

--- app/ejemplo01.py
+++ app/ejemplo01.py
@@ -2,13 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Practica01", description="Formulario", version="0.1")
 
+# Se crea una pequeña base de datos en un diccionario
 user_db = { 
 
     'jack':{'username':'jack', 'dated_joined':'2022-01-01', 'location':'New York', 'age':'28'},
     'joe':{'username':'joe', 'dated_joined':'2022-02-01', 'location':'Memphis', 'age':'25'},
     'jane':{'username':'jane', 'dated_joined':'2022-03-01', 'location':'Miami', 'age':'30'}
-
-
 }
 
 
@@ -21,4 +20,14 @@ async def ejemplo(nombres:str, apellidos:str, cellphone:str):
 
 
     return {"nombre":str(nombres), "apellido":str(apellidos), "celular":str(cellphone)}
+'''
 
+'''
+@app.get("/usuarios")
+async def users():
+
+    user_list = list(user_db.values())
+
+
+
+    return user_list
