2f914ba2551bfde9250e2bc7d8774ced05b9e6e3
fhaad <fhaad25@gmail.com>
2022-12-18 10:26:11 -0500

    cambio

--- app/ejemplo01.py
+++ app/ejemplo01.py
@@ -12,7 +12,10 @@ user_db = {
 
     'jack':{'username':'jack', 'dated_joined':'2022-01-01', 'location':'New York', 'age':'28'},
     'joe':{'username':'joe', 'dated_joined':'2022-02-01', 'location':'Memphis', 'age':'25'},
-    'jane':{'username':'jane', 'dated_joined':'2022-03-01', 'location':'Miami', 'age':'30'}
+    'jane':{'username':'jane', 'dated_joined':'2022-03-01', 'location':'Miami', 'age':'30'},
+    'peter':{'username':'peter', 'dated_joined':'2022-01-01', 'location':'New York', 'age':'32'},
+    'jhon':{'username':'jhon', 'dated_joined':'2022-02-01', 'location':'Memphis', 'age':'29'},
+    'dina':{'username':'dina', 'dated_joined':'2022-03-01', 'location':'Miami', 'age':'26'}
 }
 
 
@@ -47,6 +50,17 @@ async def usuarios(username:str):
 
 '''
 '''
+@app.get("/usuarios_consulta")
+async def users_query(limit:int):
+
+    user_list = list(user_db.values())
+
+
+
+    return user_list[:limit]
+'''
+'''
+
 @app.get("/Base_datos")
 async def BaseDatos():
     
@@ -54,8 +68,6 @@ async def BaseDatos():
 
     query = f"SELECT * FROM dataset_new" 
     df = pd.read_sql(sql=query, con=engine)
-
-
 
 
     return df.to_dict(orient="records")
